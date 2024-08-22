4eac4d667dfa7e5ae93fcbfde3d745eee488ae71
root <adam.sobieraj@op.pl>
2024-08-22 09:11:35 +0200

    refactor

--- coderdecoder/decode.py
+++ coderdecoder/decode.py
@@ -3,7 +3,7 @@ def convert_ascii_to_string(ascii_list):
 
 
 def reverse_hex_to_list(hex_string):
-    hex_pairs = [hex_string[i:i + 2] for i in range(0, len(hex_string), 2)]
+    hex_pairs = [hex_string[i:i + 2] for i in range(0, len(hex_string)-1, 2)]
     decrypted_list = [int(pair, 16) for pair in hex_pairs]
     return decrypted_list
 
@@ -20,14 +20,11 @@ def reverse_GGGGotate(movebit):
 
 def reverse_code(ListMoveBit):
     ORD_value = []
-    Index_Value = 0
-
-    for i in range(1, len(ListMoveBit)):
-        Index_Value += 1
+    Index_Value = len(ListMoveBit)-1
 
     # Reversing the loop to reconstruct the original ORD_value
     for i in range(len(ListMoveBit) - 1, 0, -1):
-        lol = reverse_GGGGotate(ListMoveBit[i]) ^ int(ListMoveBit[Index_Value - 1])
+        lol = reverse_GGGGotate(ListMoveBit[i]) ^ int(ListMoveBit[Index_Value -1])
         ORD_value.insert(0, lol)
         Index_Value -= 1
 
